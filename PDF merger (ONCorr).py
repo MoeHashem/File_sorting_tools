70ccd105c28532ac9d72d6431e1758565d322b5e
MoeHashem <moehashem97@gmail.com>
2024-06-26 09:52:26 -0400

    Created Tiff to PDF, updated Merger to pypdf

--- PDF merger (ONCorr).py
+++ PDF merger (ONCorr).py
@@ -1,46 +1,146 @@
 ####
 #UPDATE PATH BELOW TO WORKING LOCATION !!
+#pip install pypdf
 ####
 
+
+
+path = r"C:\Users\Mohammed.Hashem\OneDrive - Arup\06-Corridor Submissions\LSW Cordr Pck A 240624"
 import os
-#pip install PyPDF2 
-from PyPDF2 import PdfReader, PdfWriter, PdfMerger
+from pypdf import PdfReader, PdfWriter
 
 
-
-path = r"C:\Users\Mohammed.Hashem\OneDrive - Arup\Projects\07 - On-Corr\DMRP Reviews\\Woodbine"
-
-all_folders = [] #gives entire path names for main and sub folders
+# Collect all folder paths
+all_folders = []
 for root, dirs, files in os.walk(path):
-  for name in dirs:
-    all_folders.append(os.path.join(root, name))
+    for name in dirs:
+        all_folders.append(os.path.join(root, name))
 
+# Filter main folders
 main_folders = []
 type_filter = [".pdf", ".py", ".txt"]
-for folder in os.listdir(path): #detects all the folders in main directory(not pdf, txt, etc)
-    if not any(folder.__contains__(x) for x in type_filter):
-        main_folders.append(folder) 
+for folder in os.listdir(path):
+    if not any(folder.endswith(x) for x in type_filter):
+        main_folders.append(folder)
 
+# Collect sub-folder paths
 sub_folders = all_folders[len(main_folders):]
 
-
+# Iterate over sub-folders and merge PDFs
 for sub_folder in sub_folders:
     head, tail = os.path.split(sub_folder)
     if tail == "_Others":
+        print(f"Skipping folder: {sub_folder}")
         continue
+    
     structure_name = sub_folder.split(f"\\")[-2]
-    files_to_merge = os.listdir(sub_folder)#all files in subfolder
+    files_to_merge = os.listdir(sub_folder)
     
     merged_name = ("COMBINED_" + structure_name + "_" + tail[4:] + ".pdf").upper()
+    merged_path = os.path.join(sub_folder, merged_name)
     
-    if not os.path.exists(merged_name):#creates new merged document if one doesnt already exist
-        merger = PdfMerger()
+    if not os.path.exists(merged_path):
+        # print(f"Creating merged PDF: {merged_path}")
+        writer = PdfWriter()
+        
         for pdf in files_to_merge:
-            merger.append(open(os.path.join(sub_folder, pdf), 'rb'))
+            if pdf.lower().endswith('.pdf'):
+                pdf_path = os.path.join(sub_folder, pdf)
+                try:
+                    # print(f"Appending file: {pdf_path}")
+                    reader = PdfReader(pdf_path)
+                    for page_num in range(len(reader.pages)):
+                        writer.add_page(reader.pages[page_num])
+                except Exception as e:
+                    print(f"Failed to append {pdf_path}: {e}")
+            else:
+                print(f"Skipping non-PDF file: {pdf}")
+
+        try:
+            with open(merged_path, "wb") as fout:
+                writer.write(fout)
+            # print(f"Successfully created merged PDF: {merged_path}")
+        except Exception as e:
+            print(f"Failed to write merged PDF {merged_path}: {e}")
+    else:
+        print(f"Merged PDF already exists: {merged_path}")
+
+
+
+# import os
+# #pip install PyPDF2 
+# from PyPDF2 import PdfReader, PdfWriter, PdfMerger
+
+
+
+# path = r"C:\Users\Mohammed.Hashem\OneDrive - Arup\Projects\07 - On-Corr\DMRP Reviews\\Woodbine"
+
+# all_folders = [] #gives entire path names for main and sub folders
+# for root, dirs, files in os.walk(path):
+#   for name in dirs:
+#     all_folders.append(os.path.join(root, name))
+
+# main_folders = []
+# type_filter = [".pdf", ".py", ".txt"]
+# for folder in os.listdir(path): #detects all the folders in main directory(not pdf, txt, etc)
+#     if not any(folder.__contains__(x) for x in type_filter):
+#         main_folders.append(folder) 
+
+# sub_folders = all_folders[len(main_folders):]
+
+
+# for sub_folder in sub_folders:
+#     head, tail = os.path.split(sub_folder)
+#     if tail == "_Others":
+#         continue
+#     structure_name = sub_folder.split(f"\\")[-2]
+#     files_to_merge = os.listdir(sub_folder)#all files in subfolder
     
-        with open(f"{os.path.join(sub_folder, merged_name)}","wb") as fout:
-            merger.write(fout)
+#     merged_name = ("COMBINED_" + structure_name + "_" + tail[4:] + ".pdf").upper()
+    
+#     if not os.path.exists(merged_name):#creates new merged document if one doesnt already exist
+#         merger = PdfMerger()
+#         for pdf in files_to_merge:
+#             merger.append(open(os.path.join(sub_folder, pdf), 'rb'))
+    
+#         with open(f"{os.path.join(sub_folder, merged_name)}","wb") as fout:
+#             merger.write(fout)
 
 
 
 
+# import os
+# # pip install pypdf
+# from pypdf import PdfReader, PdfWriter, PdfMerger
+
+# path = r"C:\Users\Mohammed.Hashem\OneDrive - Arup\Projects\07 - On-Corr\11 - LSW Submission Review\06-Corridor Submissions\LSW Cordr Pck A 240624"
+
+# all_folders = []  # gives entire path names for main and sub folders
+# for root, dirs, files in os.walk(path):
+#     for name in dirs:
+#         all_folders.append(os.path.join(root, name))
+
+# main_folders = []
+# type_filter = [".pdf", ".py", ".txt"]
+# for folder in os.listdir(path):  # detects all the folders in main directory (not pdf, txt, etc)
+#     if not any(folder.__contains__(x) for x in type_filter):
+#         main_folders.append(folder)
+
+# sub_folders = all_folders[len(main_folders):]
+
+# for sub_folder in sub_folders:
+#     head, tail = os.path.split(sub_folder)
+#     if tail == "_Others":
+#         continue
+#     structure_name = sub_folder.split(f"\\")[-2]
+#     files_to_merge = os.listdir(sub_folder)  # all files in subfolder
+
+#     merged_name = ("COMBINED_" + structure_name + "_" + tail[4:] + ".pdf").upper()
+
+#     if not os.path.exists(merged_name):  # creates new merged document if one doesn't already exist
+#         merger = PdfMerger()
+#         for pdf in files_to_merge:
+#             merger.append(os.path.join(sub_folder, pdf))
+    
+#         with open(f"{os.path.join(sub_folder, merged_name)}", "wb") as fout:
+#             merger.write(fout)
---
+++ TIFF_to_PDF.py
@@ -0,0 +1,70 @@
+import os
+from PIL import Image #pip install pillow
+from pypdf import PdfWriter, PdfReader #pip install pypdf
+from tqdm import tqdm #pip install tqdm
+
+# Increase the maximum image size limit to handle large images
+Image.MAX_IMAGE_PIXELS = None
+
+def process_tiff_files_in_subfolder(subfolder_path):
+    tiff_images = []
+
+    # Collect all TIFF images in the subfolder
+    for file in os.listdir(subfolder_path):
+        if file.lower().endswith('.tiff') or file.lower().endswith('.tif'):
+            file_path = os.path.join(subfolder_path, file)
+            try:
+                # Open the image and rotate it counter-clockwise
+                img = Image.open(file_path)
+                rotated_img = img.rotate(90, expand=True)
+                tiff_images.append(rotated_img)
+            except Image.DecompressionBombError:
+                print(f"Skipped {file_path}: Image too large.")
+            except Exception as e:
+                print(f"Skipped {file_path}: {e}")
+
+    return tiff_images
+
+def save_images_as_pdf(images, output_pdf_path):
+    pdf_writer = PdfWriter()
+
+    for image in tqdm(images, desc="Processing images", unit="image"):
+        # Convert image to RGB mode (required for saving as PDF)
+        img_rgb = image.convert('RGB')
+        # Save the image temporarily as a PDF
+        img_temp_path = 'temp_image.pdf'
+        img_rgb.save(img_temp_path)
+
+        # Read the temporary PDF and add its pages to the PdfWriter
+        with open(img_temp_path, 'rb') as img_temp_file:
+            pdf_reader = PdfReader(img_temp_file)
+            for page in pdf_reader.pages:
+                pdf_writer.add_page(page)
+
+        # Remove the temporary file
+        os.remove(img_temp_path)
+
+    # Write all collected pages to the final PDF
+    with open(output_pdf_path, 'wb') as output_pdf_file:
+        pdf_writer.write(output_pdf_file)
+
+def main():
+    base_folder_path = r"C:\projectwise\can_ds01\d0123954\Metrolinx_1276_ECWE_30Jun2021_Bridges"  # Replace with the path to your folder
+
+    # Walk through all subdirectories in the base folder
+    for root, dirs, files in os.walk(base_folder_path):
+        for subfolder in tqdm(dirs, desc="Processing subfolders", unit="subfolder"):
+            subfolder_path = os.path.join(root, subfolder)
+            tiff_images = process_tiff_files_in_subfolder(subfolder_path)
+            if tiff_images:
+                # Extract the portion of the subfolder name after the hyphen and up to the first space
+                subfolder_name_parts = subfolder.split(' - ')
+                output_pdf_name = f"{subfolder_name_parts[1]}_COMBINED.pdf"
+                
+
+                output_pdf_path = os.path.join(subfolder_path, output_pdf_name)
+                save_images_as_pdf(tiff_images, output_pdf_path)
+                print(f"PDF created successfully at {output_pdf_path}")
+
+if __name__ == "__main__":
+    main()
